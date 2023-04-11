88a57fa8689cf4c27c7f07767b48df8500196045
Yoko Harada <yokolet@gmail.com>
2023-04-11 17:30:03 +0900

    updates to make input code to work

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import {fetchPlugin} from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     await esbuild.initialize({
@@ -27,12 +27,8 @@ const App = () => {
       return;
     }
 
-    // const result = await esbuild.transform(input, {
-    //   loader: 'jsx',
-    //   target: 'es2015',
-    // });
-    // console.log(result);
-    // setCode(result.code);
+    iframe.current.srcdoc = html;
+
     const result = await esbuild.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -44,28 +40,43 @@ const App = () => {
       },
     });
 
-    //console.log(result.outputFiles);
-    setCode(result.outputFiles[0].text);
-
-    try {
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   }
 
-  return (<div>
-    <textarea
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-    ></textarea>
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+`;
+
+  return (
     <div>
-      <button onClick={onClick}>Submit</button>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
-    <pre>{code}</pre>
-  </div>
   );
-}
+};
 
 ReactDOM.render(
   <App />,
