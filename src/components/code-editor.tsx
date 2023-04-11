72ca7bde1e776eb68870a4641675c0f63df09767
Yoko Harada <yokolet@gmail.com>
2023-04-11 17:57:36 +0900

    adds monaco editor

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,8 @@
+import MonacoEditor from '@monaco-editor/react';
+
+
+const CodeEditor = () => {
+  return <MonacoEditor height="500px" />;
+}
+
+export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import {useEffect, useState, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import {unpkgPathPlugin} from './plugins/unpkg-path-plugin';
 import {fetchPlugin} from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -66,6 +67,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
