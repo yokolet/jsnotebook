862a2809ce33ebf7499e83094b64e1de889abe30
Yoko Harada <yokolet@gmail.com>
2023-04-12 22:57:17 +0900

    adds better error handling

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -13,18 +13,27 @@ const bundle = async (rawCode: string) => {
     started = true;
   }
 
-  const result = await esbuild.build({
-    entryPoints: ['index.js'],
-    bundle: true,
-    write: false,
-    plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
-    define: {
-      'process.env.NODE_ENV': '"production"',
-      global: 'window',
-    },
-  });
-
-  return result.outputFiles[0].text;
+  try {
+    const result = await esbuild.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    return {
+      code: result.outputFiles[0].text,
+      err: '',
+    };
+  } catch (err: any) {
+    return {
+      code: '',
+      err: err.message,
+    };
+  }
 };
 
 export default bundle;
