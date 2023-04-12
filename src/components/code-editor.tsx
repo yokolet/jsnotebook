19d5eb0ff7b3130082537c2776e615ed6397302a
Yoko Harada <yokolet@gmail.com>
2023-04-12 15:06:50 +0900

    customizes monaco editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,33 @@
 import MonacoEditor from '@monaco-editor/react';
 
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
 
-const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  }
+  return <MonacoEditor
+    editorDidMount={onEditorDidMount}
+    value={initialValue}
+    theme="vs-dark"
+    language="javascript"
+    height="500px"
+    options={{
+      wordWrap: "on",
+      minimap: { enabled: false },
+      showUnused: false,
+      folding: false,
+      lineNumbersMinChars: 3,
+      fontSize: 16,
+      scrollBeyondLastLine: false,
+      automaticLayout: true,
+    }}
+  />;
 }
 
 export default CodeEditor;
