3a557ee37e34555900f021e297d851a58c0d540d
Yoko Harada <yokolet@gmail.com>
2023-04-12 15:07:56 +0900

    adds code formatter and bulma to monaco editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,9 @@
-import MonacoEditor from '@monaco-editor/react';
+import './code-editor.css';
+import MonacoEditor, {OnMount} from '@monaco-editor/react';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { useRef } from 'react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,28 +11,58 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
-  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
-    monacoEditor.onDidChangeModelContent(() => {
-      onChange(getValue());
-    });
-  }
-  return <MonacoEditor
-    editorDidMount={onEditorDidMount}
-    value={initialValue}
-    theme="vs-dark"
-    language="javascript"
-    height="500px"
-    options={{
-      wordWrap: "on",
-      minimap: { enabled: false },
-      showUnused: false,
-      folding: false,
-      lineNumbersMinChars: 3,
-      fontSize: 16,
-      scrollBeyondLastLine: false,
-      automaticLayout: true,
-    }}
-  />;
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
+  const onEditorDidMount: OnMount = (
+    editor:monaco.editor.IStandaloneCodeEditor,
+    monacoEditor: any,
+  ) => {
+    editorRef.current = editor;
+    editor.onDidChangeModelContent(() => onChange(editor.getValue()));
+    editor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+
+  const onFormatClick = () => {
+    if (editorRef.current) {
+      const unformatted = editorRef.current?.getValue();
+      // formatting
+      const formatted = prettier.format(unformatted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      }).replace(/\n$/, '');
+      // setting values return
+      editorRef.current?.setValue(formatted);
+    }
+  };
+
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        onMount={onEditorDidMount}
+        value={initialValue}
+        theme="vs-dark"
+        language="javascript"
+        height="500px"
+        options={{
+          wordWrap: 'on',
+          minimap: {enabled: false},
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>);
 }
 
 export default CodeEditor;
