a61b18dcd45fb0a3cb2c92a2c0ca0dad76f457e2
Yoko Harada <yokolet@gmail.com>
2023-04-14 15:07:42 +0900

    adds reducer implentations using immer

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,28 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+//import { ActionType } from './action-types';  // for manual testing
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+
+/*
+// for manual testing
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code'
+  }
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text'
+  }
+});
+
+console.log(store.getState());
+*/
