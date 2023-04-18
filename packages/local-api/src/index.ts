94eb800b07ba9baec66191a21624733046362bde
Yoko Harada <yokolet@gmail.com>
2023-04-18 23:47:48 +0900

    adds proxy to connect react app

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
+  app.use(
+    createProxyMiddleware("/", {
+      target: 'http://127.0.0.1:3000/',
+      ws: true,
+      logLevel: 'silent',
+    })
+  );
+
   return new Promise<void>((resolve, reject) => {
-    app.listen(port, resolve).on('error', reject);
+    app.listen(port, '0.0.0.0', resolve).on('error', reject);
   });
 };
