ec3aa9d5118e26b1efd9e17feaba3df514a7e1aa
Yoko Harada <yokolet@gmail.com>
2023-04-19 17:32:10 +0900

    updates react app to handle fetch and save cells

--- packages/local-client/src/state/action-creators/index.ts
+++ packages/local-client/src/state/action-creators/index.ts
@@ -8,8 +8,10 @@ import {
   MoveCellAction,
   UpdateCellAction,
 } from '../actions';
-import { CellTypes } from '../cell';
+import { Cell, CellTypes } from '../cell';
 import bundle from '../../bundler';
+import axios from 'axios';
+import { RootState } from '../reducers';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -71,3 +73,44 @@ export const createBundle = (cellId: string, input: string) => {
     });
   };
 };
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionType.FETCH_CELLS });
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get('/cells');
+      dispatch({
+        type: ActionType.FETCH_CELLS_COMPLETE,
+        payload: data,
+      })
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: ActionType.FETCH_CELLS_ERROR,
+          payload: err.message,
+        });
+      }
+    }
+  }
+};
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const { cells: { data, order } } = getState();
+
+    const cells = order.map(id => data[id]);
+
+    try {
+      await axios.post('/cells', { cells });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: ActionType.SAVE_CELLS_ERROR,
+          payload: err.message,
+        });
+      }
+    }
+    await axios.post('/cells', { cells });
+  }
+}
--- packages/local-client/src/state/store.ts
+++ packages/local-client/src/state/store.ts
@@ -1,25 +1,29 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { ActionType } from './action-types';  // for manual testing
+//import { ActionType } from './action-types';  // for manual testing
+import { persistMiddleware } from './middlewares/persist-middleware';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(persistMiddleware, thunk));
 
 
 // for manual testing
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: {
-    id: null,
-    type: 'code'
-  }
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: {
-    id: null,
-    type: 'text'
-  }
-});
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_AFTER,
+//   payload: {
+//     id: null,
+//     type: 'code'
+//   }
+// });
+//
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_AFTER,
+//   payload: {
+//     id: null,
+//     type: 'text'
+//   }
+// });
 
